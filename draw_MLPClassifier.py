77cfd0fe999bc2423fb0f36d2293b8915ed2060c
bahmanravaei <51227826+bahmanravaei@users.noreply.github.com>
2023-04-05 12:29:50 +0330

    Update draw_MLPClassifier.py

--- draw_MLPClassifier.py
+++ draw_MLPClassifier.py
@@ -31,13 +31,13 @@ def draw_neural_net(ax, left, right, bottom, top, layer_sizes, coefs_, intercept
     
     # Input-Arrows
     layer_top_0 = v_spacing*(layer_sizes[0] - 1)/2. + (top + bottom)/2.
-    for m in xrange(layer_sizes[0]):
+    for m in range(layer_sizes[0]):
         plt.arrow(left-0.18, layer_top_0 - m*v_spacing, 0.12, 0,  lw =1, head_width=0.01, head_length=0.02)
     
     # Nodes
     for n, layer_size in enumerate(layer_sizes):
         layer_top = v_spacing*(layer_size - 1)/2. + (top + bottom)/2.
-        for m in xrange(layer_size):
+        for m in range(layer_size):
             circle = plt.Circle((n*h_spacing + left, layer_top - m*v_spacing), v_spacing/8.,
                                 color='w', ec='k', zorder=4)
             if n == 0:
@@ -60,8 +60,8 @@ def draw_neural_net(ax, left, right, bottom, top, layer_sizes, coefs_, intercept
     for n, (layer_size_a, layer_size_b) in enumerate(zip(layer_sizes[:-1], layer_sizes[1:])):
         layer_top_a = v_spacing*(layer_size_a - 1)/2. + (top + bottom)/2.
         layer_top_b = v_spacing*(layer_size_b - 1)/2. + (top + bottom)/2.
-        for m in xrange(layer_size_a):
-            for o in xrange(layer_size_b):
+        for m in range(layer_size_a):
+            for o in range(layer_size_b):
                 line = plt.Line2D([n*h_spacing + left, (n + 1)*h_spacing + left],
                                   [layer_top_a - m*v_spacing, layer_top_b - o*v_spacing], c='k')
                 ax.add_artist(line)
@@ -93,7 +93,7 @@ def draw_neural_net(ax, left, right, bottom, top, layer_sizes, coefs_, intercept
             layer_top_b = v_spacing*(layer_size_b - 1)/2. + (top + bottom)/2.
         x_bias = (n+0.5)*h_spacing + left
         y_bias = top + 0.005 
-        for o in xrange(layer_size_b):
+        for o in range(layer_size_b):
             line = plt.Line2D([x_bias, (n + 1)*h_spacing + left],
                           [y_bias, layer_top_b - o*v_spacing], c='k')
             ax.add_artist(line)
@@ -112,7 +112,7 @@ def draw_neural_net(ax, left, right, bottom, top, layer_sizes, coefs_, intercept
                 
     # Output-Arrows
     layer_top_0 = v_spacing*(layer_sizes[-1] - 1)/2. + (top + bottom)/2.
-    for m in xrange(layer_sizes[-1]):
+    for m in range(layer_sizes[-1]):
         plt.arrow(right+0.015, layer_top_0 - m*v_spacing, 0.16*h_spacing, 0,  lw =1, head_width=0.01, head_length=0.02)
     # Record the n_iter_ and loss
     plt.text(left + (right-left)/3., bottom - 0.005*v_spacing, \
